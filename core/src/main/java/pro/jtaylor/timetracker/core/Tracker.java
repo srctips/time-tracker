739a468ebc1c0a9d2cb3fe8f8613f177966c9d23
Jason Taylor <jason@jasongtaylor.com>
2015-06-23 02:43:43 -0400

    Remove JavaDocs for CheckStyle warnings

--- core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
@@ -6,42 +6,23 @@ import pro.jtaylor.timetracker.core.dao.TimeEntry;
 
 import java.util.List;
 
-/**
- * Created by Jason on 6/19/2015.
- */
 @Component
 public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
 
-    /**
-     * Add method
-     * @param TimeEntry to add to entries
-     */
     public void add(TimeEntry entry) {
         entries.add(entry);
     }
 
-    /**
-     * Remove method
-     * @param TimeEntry to remove from entries
-     */
     public void remove(TimeEntry entry) {
         entries.remove(entry);
     }
 
-    /**
-     * Size method
-     * @param int size for entries
-     */
     public int size() {
         return entries.size();
     }
 
-    /**
-     * Get method
-     * @param int index for entries
-     */
     public TimeEntry get(int index) {
         return entries.get(index);
     }
